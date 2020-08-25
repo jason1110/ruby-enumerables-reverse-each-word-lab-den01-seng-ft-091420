674e7db080d25b626c9930158c7080c3516fb193
Jason Easterly <jason.easterly11@gmail.com>
2020-08-25 19:44:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,21 +1,18 @@
-require 'pry'
-#hi = "Hello! How are you?"
-
 
 def reverse_each_word(string)
-  array = string.split(" ")
-  reversed = []
-  
+array = string.split(" ")
+reversed = []
+
+#.each enumerator  
 #  array.each do |arr|
 #    reversed << arr.reverse
 #  end  
  #reversed.join(" ")
  #end
- 
- 
+
+#.colelct enumerator 
  array.collect do |arr| 
    reversed  << arr.reverse
   end 
  reversed.join(" ")
  end
- #reverse_each_word(hi)
