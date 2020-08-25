bc71393c042414dbefc8af6ce633d7b30802a3c6
Jason Easterly <jason.easterly11@gmail.com>
2020-08-25 19:40:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,15 +4,15 @@ require 'pry'
 
 def reverse_each_word(string)
   array = string.split(" ")
-  reversed = []
+#  reversed = []
   
-  array.each do |arr|
-    reversed << arr.reverse
-  end  
- reversed.join(" ")
- end
+#  array.each do |arr|
+#    reversed << arr.reverse
+#  end  
+ #reversed.join(" ")
+ #end
  
  
  array.collect{|arr| arr.reverse}
- 
+ array.join(" ")
  #reverse_each_word(hi)
